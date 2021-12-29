3a99e558bfb3d19cf20ea2b7ec3906204f307ae5
Xicad <65637689+Xicad@users.noreply.github.com>
2021-12-28 22:45:13 -0800

    Update MethodsReturnNonnullByDefault.java

--- src/main/java/io/github/opencubicchunks/cubicchunks/annotation/MethodsReturnNonnullByDefault.java
+++ src/main/java/io/github/opencubicchunks/cubicchunks/annotation/MethodsReturnNonnullByDefault.java
@@ -1,17 +1,8 @@
-package io.github.opencubicchunks.cubicchunks.annotation;
+package io.github.opencubicchunks.cubicchunks.chunk;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+public interface VerticalViewDistanceListener {
 
-import javax.annotation.Nonnull;
-import javax.annotation.meta.TypeQualifierDefault;
+    void setIncomingVerticalViewDistance(int verticalDistance);
 
-@Documented
-@Nonnull
-@TypeQualifierDefault(ElementType.METHOD) // Note: This is a copy of javax.annotation.ParametersAreNonnullByDefault with target changed to METHOD
-@Retention(RetentionPolicy.RUNTIME)
-public @interface MethodsReturnNonnullByDefault {
-
+    int getVerticalViewDistance();
 }
