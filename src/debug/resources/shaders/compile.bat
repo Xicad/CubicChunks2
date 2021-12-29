151ceb6a1a14ceed2e7acd20186b0f7d8639b698
Xicad <65637689+Xicad@users.noreply.github.com>
2021-12-28 23:20:54 -0800

    Delete src/debug directory

--- src/debug/resources/shaders/compile.bat
+++
@@ -1,3 +0,0 @@
-G:/Comptuer/VulkanSDK/1.2.141.2/Bin32/glslc.exe -fshader-stage=vert vertex.glsl -o vert.spv
-G:/Comptuer/VulkanSDK/1.2.141.2/Bin32/glslc.exe -fshader-stage=frag fragment.glsl -o frag.spv
-pause
